5d7517b12cdeae3e6a793ab354720012a7a5144c
lvyuanxin <lvyuanxin@wps.cn>
2026-06-25 23:21:23 +0800

    feat: add opencode.nvim and snacks.nvim configuration

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -34,6 +34,13 @@ return {
 
   {
     "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lua",
+      "saadparwaiz1/cmp_luasnip",
+      "https://codeberg.org/FelipeLema/cmp-async-path.git",
+    },
     opts = {
       mapping = {
         ["<C-k>"] = require("cmp").mapping.select_prev_item(),
@@ -96,5 +103,88 @@ return {
         virt_text_pos = "eol",
       },
     },
+  },
+
+  {
+    "folke/snacks.nvim",
+    lazy = false,
+    opts = {
+      input = { enabled = true },
+      terminal = { enabled = true },
+      picker = {
+        enabled = true,
+        layout = {
+          preset = "default",
+          layout = {
+            width = 0.7,
+            height = 0.6,
+          },
+        },
+      },
+    },
+  },
+
+  {
+    "nickjvandyke/opencode.nvim",
+    version = "*",
+    lazy = false,
+    config = function()
+      vim.o.autoread = true
+
+      local opencode_cmd = "opencode --port"
+      local snacks_terminal_opts = {
+        win = {
+          position = "right",
+          enter = false,
+        },
+      }
+
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {
+        server = {
+          start = function()
+            require("snacks.terminal").open(opencode_cmd, snacks_terminal_opts)
+          end,
+        },
+      }
+
+      -- 快捷键设置
+      local map = vim.keymap.set
+
+      -- Toggle OpenCode 侧边栏
+      map({ "n", "t" }, "<C-.>", function()
+        require("snacks.terminal").toggle(opencode_cmd, snacks_terminal_opts)
+      end, { desc = "Toggle OpenCode" })
+
+      -- Ask OpenCode
+      map({ "n", "x" }, "<leader>oa", function()
+        require("opencode").ask("@this: ")
+      end, { desc = "Ask OpenCode" })
+
+      -- Select OpenCode
+      map({ "n", "x" }, "<leader>os", function()
+        require("opencode").select()
+      end, { desc = "Select OpenCode" })
+
+      -- Operator: 追加范围到 OpenCode
+      map({ "n", "x" }, "go", function()
+        return require("opencode").operator("@this ")
+      end, { desc = "Append range to OpenCode", expr = true })
+
+      -- 追加整行到 OpenCode
+      map("n", "goo", function()
+        return require("opencode").operator("@this ") .. "_"
+      end, { desc = "Append line to OpenCode", expr = true })
+
+      -- 向上滚动 OpenCode
+      map("n", "<S-C-u>", function()
+        require("opencode").command("session.half.page.up")
+      end, { desc = "Scroll OpenCode up" })
+
+      -- 向下滚动 OpenCode
+      map("n", "<S-C-d>", function()
+        require("opencode").command("session.half.page.down")
+      end, { desc = "Scroll OpenCode down" })
+    end,
   },
 }
